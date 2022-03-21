a45bfa7313313db491a6e50e38b06b7f7d04a73c
ababa555 <japan14144@yahoo.co.jp>
2022-03-21 12:54:04 +0900

    initial commit

--- src/pokemon_api/pokemon_api/settings/base.py
+++ src/pokemon_api/pokemon_api/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yg5y!t5l%tpi%3zspa6)k!1x22@^^@m^f@8!0#$5%z$!0xuzcy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ src/pokemon_api/pokemon_api/settings/local.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from pokemon_api.settings.base import *
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
